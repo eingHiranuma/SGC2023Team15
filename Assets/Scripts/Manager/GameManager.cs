0e28771bbe2ca7770e2a6bc8bf5670aa16d297da
eingHiranuma <sgc2023.hira@gmail.com>
2023-10-07 20:00:59 +0900

    プレイヤー・敵をマネージャ管理する

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,62 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+	private enum phase
+	{
+		title_init,
+		title,
+		game_init,
+		game,
+		result,
+	}
+
+	private phase nowPhase = phase.title_init;
+
+	/// <summary>
+	/// Start
+	/// </summary>
+	protected override void Start()
+	{
+		// サウンド初期化
+		SoundMasterData.Setup();
+		SoundManager.Instance.Initialize();
+
+		PlayerManager.Instance.LoadPlayer(Vector3.zero);
+
+        EnemyManager.Instance.SetNest(new Vector3(10, 0, 0));
+        EnemyManager.Instance.SetNest(new Vector3(10, 10, 0));
+    }
+
+    /// <summary>
+    /// Update
+    /// </summary>
+    protected override void Update()
+	{
+		switch (nowPhase)
+		{
+			// タイトル初期化
+			case phase.title_init:
+				break;
+
+			// タイトル
+			case phase.title:
+				break;
+
+			// ゲーム初期化
+			case phase.game_init:
+				break;
+
+			// ゲームメイン
+			case phase.game:
+				break;
+
+            // リザルト
+            case phase.result:
+                break;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Manager/PlayerManager.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : SingletonMonoBehaviour<PlayerManager>
+{
+    /// <summary>
+    /// プレイヤー
+    /// </summary>
+    private GunPlayer player = null;
+	public GunPlayer Player => player;
+
+
+	/// <summary>
+	/// Start
+	/// </summary>
+	protected override void Start()
+	{
+	}
+
+
+    /// <summary>
+    /// プレイヤーロード
+    /// </summary>
+	public void LoadPlayer(Vector3 position)
+	{
+		var go = Instantiate(Resources.Load("Player/Player") as GameObject);
+		go.name = "Player";
+		go.transform.position = position;
+		player = go.GetComponent<GunPlayer>();
+	}
+}
+
